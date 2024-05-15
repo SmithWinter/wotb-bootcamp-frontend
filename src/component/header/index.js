28f620f3ff3ccf809cb443b1a9365b50b8dd032c
Smith Winter <hovuminhduc@gmail.com>
2024-05-15 18:16:49 +0700

    Day 2 WIP

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,47 +1,22 @@
 import React from 'react';
 
-import Button from '@mui/material/Button';
-import { styled } from '@mui/system';
-
+import logo from '../../assets/images/VGF_Icon.jpg'
 
 import './style.css'
 
 const Header = () => {
     return (
         <div className='header-container'>
-            <div className='header-title'>
-                WOTB Bootcamp by VGF Clan
+            <div className='header-intro'>
+                <img src={logo} alt='header-icon' width={100} height={100} className='intro-image'/>
+                <div className='header-title'>
+                    WOTB Bootcamp by VGF Clan
+                </div>
             </div>
             <div className='header-navigation'>
-                <Button color="primary" variant='contained'  size='large' to="#home"
-                    sx={{
-                        marginRight: 1
-                    }}
-                >
-                    Home
-                </Button>
-                <Button color="secondary" variant='contained'  size='large' to="#about"> 
-                    About
-                </Button>
-                <Button color="info" variant='contained'  size='large' to="#services">
-                    Services
-                </Button>
-                <Button color="success" variant='contained'  size='large' to="#contact">
-                    Contact
-                </Button>
+
             </div>
         </div>
-        // <AppBar position="static">
-        // <Toolbar>
-        //     <Typography variant="h6" style={{ flexGrow: 1 }}>
-        //     World of Tank Blitz Bootcamp  - VGF Clan
-        //     </Typography>
-            // <Button color="inherit" component={Link} to="#home">Home</Button>
-            // <Button color="inherit" component={Link} to="#about">About</Button>
-            // <Button color="inherit" component={Link} to="#services">Services</Button>
-            // <Button color="inherit" component={Link} to="#contact">Contact</Button>
-        // </Toolbar>
-        // </AppBar>
     );
 };
 
